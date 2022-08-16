efc6158ad8eac2dfc6564d681b7add30b4b67191
Jorge Capona <jcapona@gmail.com>
2022-08-16 11:15:57 -0400

    Check server status in e2e tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -4,6 +4,7 @@ from os import environ
 from tkinter import Tk
 
 import pytest
+import requests
 from mock import AsyncMock
 
 from pt_web_vnc.vnc import (
@@ -24,12 +25,17 @@ async def test_async_e2e():
     await asyncio.sleep(1)
 
     details = await async_connection_details(display_id=100)
-    print(details.url)
 
+    # URL has the correct format
     regex = (
         r"http\:\/\/[\-\.a-zA-Z0-9]+:61100\/vnc\.html\?autoconnect=true&resize=scale"
     )
     assert re.match(regex, details.url)
+
+    # Server is running
+    url = f"{details.scheme}://localhost:{details.port}{details.path}"
+    r = requests.get(url)
+    assert r.status_code == 200
 
     assert callback.call_count == 0
 
@@ -38,17 +44,27 @@ async def test_async_e2e():
     Tk()
 
     await asyncio.sleep(2)
+
+    # Callback was called after display activity
     assert callback.call_count == 1
 
     await async_stop(display_id=100)
 
     await asyncio.sleep(1)
 
+    # Server isn't running
+    with pytest.raises(requests.exceptions.ConnectionError):
+        r = requests.get(url)
+
 
 def test_e2e():
     start(display_id=50)
 
     details = connection_details(display_id=50)
+    # Server is running
+    url = f"{details.scheme}://localhost:{details.port}{details.path}"
+    r = requests.get(url)
+    assert r.status_code == 200
 
     regex = (
         r"http\:\/\/[\-\.a-zA-Z0-9]+:61050\/vnc\.html\?autoconnect=true&resize=scale"
@@ -56,3 +72,7 @@ def test_e2e():
     assert re.match(regex, details.url)
 
     stop(display_id=50)
+
+    # Server isn't running
+    with pytest.raises(requests.exceptions.ConnectionError):
+        r = requests.get(url)
